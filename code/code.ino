0ccb8e436faa5bf5c00677006f3291b33fe62e61
EHMD28 <ehmdiagne0530@gmail.com>
2025-12-08 21:59:04 -0500

    added game state

--- code/code.ino
+++ code/code.ino
@@ -1,26 +1,56 @@
+namespace Colors {
+    enum ColorType : uint8_t {
+        NONE,
+        RED,
+        BLUE,
+        YELLOW,
+        GREEN
+    };
+};
+
 namespace Buttons {
-    /* The value of the enum variant corresponds to the pin. Pin 0 (RX) and pin
-    1 (TX) should not be connected to anything, because it interferes with
+    /* The value of the enum variant corresponds to the pin. PinType 0 (RX) and
+    pin 1 (TX) should not be connected to anything, because it interferes with
     uploading code. */
-    enum Pin : uint8_t {
+    enum PinType : uint8_t {
         RED = 2,
         BLUE = 3,
         YELLOW = 4,
         GREEN = 5,
     };
+
+    constexpr Buttons::PinType buttons[] = {
+        Buttons::RED,
+        Buttons::BLUE,
+        Buttons::YELLOW,
+        Buttons::GREEN
+    };
+    constexpr int num_buttons = sizeof(buttons) / sizeof(buttons[0]);
 };
 
 namespace Leds {
     // The value of the enum variant corresponds to the pin.
-    enum Pin : uint8_t {
+    enum PinType : uint8_t {
         RED = 6,
         BLUE = 7,
         YELLOW = 8,
         GREEN = 9
     };
+
+    Leds::PinType pin_of(Colors::ColorType color) {
+        switch (color) {
+            case Colors::RED: return Leds::RED;
+            case Colors::BLUE: return Leds::BLUE;
+            case Colors::YELLOW: return Leds::YELLOW;
+            case Colors::GREEN: return Leds::GREEN;
+            /* This line of code should never run, but it's necessary for the
+        function to return a value for every input.  */
+            default: return (Leds::PinType) 0;
+        }
+    }
 };
 
-Leds::Pin button_to_led(Buttons::Pin pin) {
+Leds::PinType button_to_led(Buttons::PinType pin) {
     switch (pin) {
         case Buttons::RED: return Leds::RED;  
         case Buttons::BLUE: return Leds::BLUE;  
@@ -28,11 +58,36 @@ Leds::Pin button_to_led(Buttons::Pin pin) {
         case Buttons::GREEN: return Leds::GREEN;
         /* This line of code should never run, but it's necessary for the
         function to return a value for every input.  */
-        default: return (Leds::Pin) 0;  
+        default: return (Leds::PinType) 0;  
     }
 }
 
+struct GameState {
+    /* The longest a sequence can be is 256 colors, because that is the highest
+    value which can be stored in an unsigned 8 bit integer like round. */
+    Colors::ColorType sequence[UINT8_MAX];
+    uint8_t round;
+};
+
+/* This is the global struct used for representing the current game state. */
+struct GameState game_state;
+
+/* Initializes the values of the global game_state struct. This function MUST be
+called in setup(), or else game_state will be using garbage values from memory.
+*/
+void initalize_game_state() {
+    game_state.round = 0;
+    /* TODO: Once the rest of the project is finished, check if this
+    initialization is necessary. */
+    for (int i = 0; i < UINT8_MAX; i++) {
+        game_state.sequence[i] = Colors::NONE;
+    }
+}
+
+bool
+
 void setup() {
+    initalize_game_state();
     /* Button pins should be configured as INPUT_PULLUP. A HIGH value indicates 
     the button isn't being pushed, and a LOW value indicates that it is being
     pushed. */
@@ -48,21 +103,13 @@ void setup() {
     pinMode(Leds::GREEN, OUTPUT);
 }
 
-constexpr Buttons::Pin buttons[] = {
-    Buttons::RED,
-    Buttons::BLUE,
-    Buttons::YELLOW,
-    Buttons::GREEN
-};
-constexpr int num_buttons = sizeof(buttons) / sizeof(buttons[0]);
-
 void loop() {
-    for (int i = 0; i < num_buttons; i++) {
-        Leds::Pin ledPin = button_to_led(buttons[i]);
-        if (digitalRead(buttons[i]) == LOW) {
-            digitalWrite(ledPin, HIGH);
+    for (int i = 0; i < Buttons::num_buttons; i++) {
+        Leds::PinType ledPinType = button_to_led(Buttons::buttons[i]);
+        if (digitalRead(Buttons::buttons[i]) == LOW) {
+            digitalWrite(ledPinType, HIGH);
         } else {
-            digitalWrite(ledPin, LOW);
+            digitalWrite(ledPinType, LOW);
         }
     }
 }
